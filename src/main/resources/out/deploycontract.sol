e701166fec3a1e17e2f6e864f1e783fdd6ec8379
jensenyang2004 <82329310+jensenyang2004@users.noreply.github.com>
2021-08-04 17:02:37 +0800

    Update deploycontract.sol

--- src/main/resources/out/deploycontract.sol
+++ src/main/resources/out/deploycontract.sol
@@ -12,9 +12,13 @@ contract deploycontract{
         require(msg.sender==owner);
         _;
     }
+    //給予權限
     function depoy_contract() public onlyowner{
         electioncontract = new editedelection(owner);
+        //在子合約中的建構子要求發布時輸入管理者地址
     }
+    //呼叫此方法就會發佈新的子合約
+    //此方法限制管理者使用
     function return_address() public view returns(address){
         return address(electioncontract);
     }
